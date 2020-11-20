9855b20667117003220badf8aada2f680df18bc9
Dwigth Astacio <dwigth32@outlook.es>
2020-11-20 16:44:24 -0600

    🚀v0.3 Ahora la aplicación ya tiene persistencia de monitoreo, desde que inicia el proceso, ya no solo cuando se conectan los clientes.

--- server.ts
+++ server.ts
@@ -1,3 +1,4 @@
+const dash = require('appmetrics-dash');
 import { Socket } from 'socket.io';
 import * as express from 'express';
 import { Application } from 'express';
@@ -21,6 +22,16 @@ import { MonitorErrorsCatalog } from './entities/MonitorErrorsCatalog';
 import { MonitorSystemsErrors } from './entities/MonitorSystemsErrors';
 import { MonitoredWebService } from './entities/MonitoredWebService';
 
+/**
+ * ================================================
+ * 
+ *  Variables globales
+ * 
+ * ================================================
+ */
+
+let _SYSTEMS: MonitoredSystem[] = [];
+
 createConnection().then(async connection => {
     const config = await getRepository(MonitorConfiguration).findOne({ where: { activated: 1 } });
     const systems = await getRepository(MonitoredSystem).find({ relations: ['websites', 'databases', 'webservices', 'errors'] })
@@ -30,115 +41,123 @@ createConnection().then(async connection => {
     const io = socketio(http);
     const FETCH_INTERVAL = config.timeInterval || 10000;
 
+    // Enviamos por primera vez los datos al usuario
+    _SYSTEMS = systems;
+
     // Middlewares
     app.use(cors())
 
+    // Proceso de monitoreo
+
+    setInterval(async () => {
+        const systems = await getRepository(MonitoredSystem).find({ relations: ['websites', 'databases', 'webservices', 'errors'] })
+        const systemPromises = systems.map(async system => {
+            // console.log(system);
+            _SYSTEMS = [];
+
+            // Si hay sitios web accedemos a ellos
+            let websites = (system.websites != null) ? await Promise.all(system.websites.map(async website => {
+                // Accediendo a estatuses de las páginas web
+                const response = {
+                    name: website.name,
+                    url: website.url,
+                    statusResponseCode: 0,
+                }
+                const axiosResponse = await axios.get(website.url).catch(e => e.response);
+                response.statusResponseCode = axiosResponse.status;
+                await ErrorHandler(system, response.statusResponseCode, website.name);
+                return response;
+
+            })) : [];
+
+            let webservices = (system.webservices != null) ? await Promise.all(system.webservices.map(async webservice => {
+                // Accediendo a estatuses de las páginas web
+                webservice.system = system;
+                const response = {
+                    name: webservice.name,
+                    url: webservice.url,
+                    statusResponseCode: 0,
+                }
+                const axiosResponse: AxiosResponse = await axios.get(webservice.url, { headers: { token: webservice.token } }).catch(e => e.response);
+                response.statusResponseCode = axiosResponse.status;
+                try {
+                    await AnalizingWebServiceContent(webservice, axiosResponse.data);
+                } catch (error) {
+                    console.log(error);
+                }
+                await ErrorHandler(system, response.statusResponseCode, webservice.name);
+                return response;
+
+            })) : [];
+
+            let databases = (system.databases != null) ? await Promise.all(system.databases.map(async database => {
+                // console.log(database);
+                let isConnected = false;
+                try {
+                    const connectionManager = new ConnectionManager();
+                    let postgres = {
+                        //@ts-ignore
+                        type: database.type,
+                        host: database.host,
+                        port: database.port,
+                        username: database.username,
+                        password: database.password,
+                        database: database.databaseName,
+                    };
+                    let oracle = {
+                        //@ts-ignore
+                        type: database.type,
+                        host: database.host,
+                        port: database.port,
+                        sid: database.sid,
+                        username: database.username,
+                        password: database.password,
+                        database: database.databaseName,
+                    }
+                    let connection;
+                    if (database.type === 'postgres') {
+                        // @ts-ignore
+                        connection = connectionManager.create(postgres);
+                    } else if (database.type === 'oracle') {
+                        // @ts-ignore
+                        connection = connectionManager.create(oracle);
+                    }
+                    const connectResult = await connection.connect();
+                    isConnected = connectResult.isConnected;
+                    await connectResult.close();
+                } catch (error) {
+                    console.log(error);
+                }
+
+                const response = {
+                    name: database.name,
+                    url: database.host,
+                    statusResponseCode: (isConnected) ? 200 : 500,
+                }
+                await ErrorHandler(system, response.statusResponseCode, database.name);
+                return response;
+
+            })) : [];
+
+            return {
+                id: system.id,
+                systemName: system.systemName,
+                upDate: system.upDate,
+                websites,
+                webservices,
+                databases,
+                errors: system.errors
+            }
+        });
+
+        _SYSTEMS = await Promise.all(systemPromises).then(data => data.map(urlResponse => urlResponse)).catch(error => error)
+        io.emit('websites-info', { responses: _SYSTEMS })
+
+    }, FETCH_INTERVAL);
+
     // Conexión de socket.io
     io.on('connection', (socket: Socket) => {
-
-        setInterval(async () => {
-
-            const systemPromises = systems.map(async system => {
-                // console.log(system);
-
-                // Si hay sitios web accedemos a ellos
-                let websites = (system.websites != null) ? await Promise.all(system.websites.map(async website => {
-                    // Accediendo a estatuses de las páginas web
-                    const response = {
-                        name: website.name,
-                        url: website.url,
-                        statusResponseCode: 0,
-                    }
-                    const axiosResponse = await axios.get(website.url).catch(e => e.response);
-                    response.statusResponseCode = axiosResponse.status;
-                    await ErrorHandler(system, response.statusResponseCode, website.name);
-                    return response;
-
-                })) : [];
-
-                let webservices = (system.webservices != null) ? await Promise.all(system.webservices.map(async webservice => {
-                    // Accediendo a estatuses de las páginas web
-                    webservice.system = system;
-                    const response = {
-                        name: webservice.name,
-                        url: webservice.url,
-                        statusResponseCode: 0,
-                    }
-                    const axiosResponse: AxiosResponse = await axios.get(webservice.url, { headers: { token: webservice.token } }).catch(e => e.response);
-                    response.statusResponseCode = axiosResponse.status;
-                    try {
-                        await AnalizingWebServiceContent(webservice, axiosResponse.data);
-                    } catch (error) {
-                        console.log(error);
-                    }
-                    await ErrorHandler(system, response.statusResponseCode, webservice.name);
-                    return response;
-
-                })) : [];
-
-                let databases = (system.databases != null) ? await Promise.all(system.databases.map(async database => {
-                    // console.log(database);
-                    let isConnected = false;
-                    try {
-                        const connectionManager = new ConnectionManager();
-                        let postgres = {
-                            //@ts-ignore
-                            type: database.type,
-                            host: database.host,
-                            port: database.port,
-                            username: database.username,
-                            password: database.password,
-                            database: database.databaseName,
-                        };
-                        let oracle = {
-                            //@ts-ignore
-                            type: database.type,
-                            host: database.host,
-                            port: database.port,
-                            sid: database.sid,
-                            username: database.username,
-                            password: database.password,
-                            database: database.databaseName,
-                        }
-                        let connection;
-                        if (database.type === 'postgres') {
-                            // @ts-ignore
-                            connection = connectionManager.create(postgres);
-                        } else if (database.type === 'oracle') {
-                            // @ts-ignore
-                            connection = connectionManager.create(oracle);
-                        }
-                        const connectResult = await connection.connect();
-                        isConnected = connectResult.isConnected;
-                        await connectResult.close();
-                    } catch (error) {
-                        console.log(error);
-                    }
-
-                    const response = {
-                        name: database.name,
-                        url: database.host,
-                        statusResponseCode: (isConnected) ? 200 : 500,
-                    }
-                    await ErrorHandler(system, response.statusResponseCode, database.name);
-                    return response;
-
-                })) : [];
-
-                return {
-                    id: system.id,
-                    systemName: system.systemName,
-                    upDate: system.upDate,
-                    websites,
-                    webservices,
-                    databases,
-                    errors: system.errors
-                }
-            });
-
-            const responses = await Promise.all(systemPromises).then(data => data.map(urlResponse => urlResponse)).catch(error => error)
-            socket.emit('websites-info', { responses });
-        }, FETCH_INTERVAL);
+        socket.emit('websites-info', { responses: _SYSTEMS })
         console.log('Se ha conectado el cliente.');
     });
 
@@ -148,6 +167,8 @@ createConnection().then(async connection => {
     http.listen(3009, () => {
         console.log('listening on *:3009');
     });
+
+    dash.monitor({ server: http });
 
 }).catch(error => console.log(error));
 

ca36cede7111110271325fbef180dbf37423c739
Dwigth Astacio Hernández <dwigth32@outlook.es>
2021-05-21 14:32:48 -0500

    Tabla de contenido de los webservices

--- src/app/modulos/config/pages/index/pages/webservices/components/ws-table/ws-table.component.ts
+++ src/app/modulos/config/pages/index/pages/webservices/components/ws-table/ws-table.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Subscription } from 'rxjs';
+import { IMonitoredWebservice } from 'src/app/interfaces/core';
+import { ConfigService } from 'src/app/modulos/config/config-service.service';
 
 @Component({
   selector: 'app-ws-table',
@@ -7,9 +11,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class WsTableComponent implements OnInit {
 
-  constructor() { }
+  private sub: Subscription;
+  private systemId:number;
+  public Webservices:IMonitoredWebservice[];
+  constructor(private route: ActivatedRoute, private configS:ConfigService) { }
 
   ngOnInit(): void {
-  }
+    this.sub = this.route.params.subscribe(params => {
+      this.systemId = +params['systemId'];
+      if(this.systemId) {
+        this.configS.GetWebservices(this.systemId).subscribe(data => this.Webservices = data.data)
+      } else {
+        this.configS.GetWebservices().subscribe(data => this.Webservices = data.data)
+      }
+   });
+ }
+
+ ngOnDestroy() {
+   this.sub.unsubscribe();
+ }
 
 }

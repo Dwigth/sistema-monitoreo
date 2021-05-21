4a0ca02038d4e415384652a93100019a33b7facf
Dwigth Astacio Hernández <dwigth32@outlook.es>
2021-05-21 14:37:19 -0500

    Tabla de páginas web

--- src/app/modulos/config/pages/index/pages/websites/components/w-table/w-table.component.ts
+++ src/app/modulos/config/pages/index/pages/websites/components/w-table/w-table.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Subscription } from 'rxjs';
+import { IMonitoredWebsite } from 'src/app/interfaces/core';
+import { ConfigService } from 'src/app/modulos/config/config-service.service';
 
 @Component({
   selector: 'app-w-table',
@@ -7,9 +11,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class WTableComponent implements OnInit {
 
-  constructor() { }
+  private sub: Subscription;
+  private systemId:number;
+  public Websites:IMonitoredWebsite[];
+  constructor(private route: ActivatedRoute, private configS:ConfigService) { }
 
   ngOnInit(): void {
-  }
+    this.sub = this.route.params.subscribe(params => {
+      this.systemId = +params['systemId'];
+      if(this.systemId) {
+        this.configS.GetWebsites(this.systemId).subscribe(data => this.Websites = data.data)
+      } else {
+        this.configS.GetWebsites().subscribe(data => this.Websites = data.data)
+      }
+   });
+ }
+
+ ngOnDestroy() {
+   this.sub.unsubscribe();
+ }
 
 }
